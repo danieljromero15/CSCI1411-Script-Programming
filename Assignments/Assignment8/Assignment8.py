118ddb3e5c2c223f09f8a1facd35f4046f26a478
Daniel Romero <danieljromero305@gmail.com>
2024-11-07 10:35:27 -0500

    Completed Assignment 8

--- Assignments/Assignment8/Assignment8.py
+++ Assignments/Assignment8/Assignment8.py
@@ -1,14 +1,14 @@
-import urllib.request
-import json
-import datetime
-import os
 import re
-import azuremaps
+
+from azuremaps import getCountry
 
 ########## Global Variables ##########
 # 1. Three Python list objects will be required to store our data found.
 # Create 3 empty python lists named 'ips','unique_ips', and 'toJson'
 # YOUR CODE HERE (3 lines)
+ips = []
+unique_ips = []
+toJson = []
 
 
 
@@ -17,6 +17,7 @@ import azuremaps
 # in logs/access.log
 # Create with variable name "file". Read mode is only needed
 # YOUR CODE HERE (1 line)
+file = open('logs/access.log', 'r')
 
 
 # 3. You will need to iterate through the log file line by line
@@ -26,22 +27,28 @@ import azuremaps
 # You will need to use the 'extend' method to add each ip list 
 # to the new list 'ips'
 # YOUR CODE HERE (< 5 lines)
+ip_pattern = "((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}" #unashamedly stole this from stackoverflow, any version I would make would be twice as long at a minimum
+for line in file:
+    re_test = re.search(ip_pattern, line)
+    if re_test is not None:
+        ips.append(re_test.group(0)) # why extend? why does each individual ip need its own list?
 
 
 # 4. Now that ips is populated, you will need to populate a new list
 # called unique_ips removing all duplicates. One method is to convert 
 # the ips list to a set and then back to a list. Example: list(set(ips))
 # YOUR CODE HERE (1 line)
-
+unique_ips = list(set(ips))
 
 
 # HINT: The number of total IPs found in the log file should return
 # length of 150. The number of unique IPs found should return: 135
 # Use print statements to validate your lists. It is recommended 
-# validating your list lenghts before proceeding...
+# validating your list lengths before proceeding...
 #print(ips)
 #print(len(ips))
 #print(len(unique_ips))
+# these are giving the correct values considering I already excluded the invalid ones that are just gonna spit out a 400 error anyways
 
 
 
@@ -50,26 +57,26 @@ import azuremaps
 # return a country ISO code for each IP in the unique_ips list.
 # Write a for loop to iterate through the unique_ips list.
 # YOUR CODE HERE (1 line)
-
+for ip in unique_ips:
     
     # 6. A variable user_country will be needed to store the country value.
     # Make a call to getCountry function in azuremaps. 
     # YOUR CODE HERE (1 line)
-
+    user_country = getCountry(ip)
 
     # IMPORTANT: Uncomment the three tick marks below and at the end
     # of the code to complete rest of the assignment.
-'''
+
     # 7. Complete the if statement below to build a 
     # dictionary with two key:value pairs. "user" will be a single ip address provided
     # through a variable. "country" will be the country code returned in a variable 
-    # you recentry created.
+    # you recently created.
     # Lastly, the formated user will need to be appended to the toJson list. 
     # YOUR CODE HERE (2 variables and method name)
     if user_country:
-        user = {"user": #Variable, 
-                "country": #Variable}
-        toJson.#method#(user)
+        user = {"user": ip,
+                "country": user_country}
+        toJson.append(user)
 
 
 # Lastly, we must build the found.js file to be used with
@@ -88,11 +95,10 @@ print("Check your index.html file for the results!")
 
 # 8. How many countries were identified with attacker addresses?
 # Answer
-answer8=0
+answer8=9
 # 9. Which country contained the most unique attacker addresses?
 # Answer
-answer9=0
+answer9="RU"
 # 10. How many total attackers have been identified?
 # Answer
-answer10=0
-'''
+answer10=124
--- Assignments/Assignment8/azuremaps.py
+++ Assignments/Assignment8/azuremaps.py
@@ -1,25 +1,34 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # 1. Insert API Key as a string that is provided by Instructor in D2L
 # YOUR CODE HERE (1 line)
-key=
+# I'm not committing your API key to GitHub, you're welcome
+key=os.getenv("AZURE_KEY")
 
-# 2. getCountry function is used to lookup the appropriate country code
+# 2. getCountry function is used to look up the appropriate country code
 # assigned to the IP Address. This function requires one argument (ip)
 # You will need to insert the key and ip variables to complete the apiURL string
 # Example API URL: 'https://atlas.microsoft.com/geolocation/ip/json?subscription-key=<key inside D2L>&api-version=1.0&ip=###.###.###.###
 def getCountry(ip):
-    apiURL = #YOUR CODE HERE
-    r = #YOUR CODE HERE
+    apiURL = f'https://atlas.microsoft.com/geolocation/ip/json?api-version=1.0&subscription-key={key}&ip={ip}'
+    r = requests.get(apiURL)
     
     # Print statements are extremely helpful!
     # OPTIONAL Print statements can be used here
     #print(r.json())
 
-    # 3. Some requests requests returned may not be what we expect. We must add an exception handling
-    # statement to avoid errors that may prevent all of the IPs from being processed.
+    # 3. Some requests returned may not be what we expect. We must add an exception handling
+    # statement to avoid errors that may prevent all the IPs from being processed.
     # You need to extract the country code from the JSON returned.
     # YOUR CODE HERE (Less than 10 lines)
+    if r.status_code == 200:
+        return r.json()['countryRegion']['isoCode']
+    else:
+        print(f"Error {r.status_code} for {ip}")
 
 
 
